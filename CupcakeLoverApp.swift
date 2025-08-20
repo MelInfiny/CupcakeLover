41daa345c2838676c0b6f587e19e47fc21af6d98
MelInfiny <pb20ar35@outlook.fr>
2025-08-20 16:34:44 +0200

    initial app from 100 days

---
+++ CupcakeLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeLoverApp.swift
+//  CupcakeLover
+//
+//  Created by Emilie NOLBAS on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
